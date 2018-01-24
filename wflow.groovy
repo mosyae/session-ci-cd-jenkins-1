5ac7b48c37af6cc713420f85f996738447b3e706
arie.belenky <ariebelenky@Aries-MacBook-Pro.local>
2018-01-24 13:40:57 +0200

    brought back parallel execution on different node

--- wflow.groovy
+++ wflow.groovy
@@ -1,33 +1,74 @@
-node ('opsschool-slaves'){
-    currentBuild.result = "SUCCESS"
+parallel(
+        "LocalStream":{
+            node ('opsschool-slaves'){
+                currentBuild.result = "SUCCESS"
 
-    stage('Checkout'){
+                stage('Checkout'){
 
-        checkout scm
-    }
+                    checkout scm
+                }
 
-    stage('Test'){
-        sh 'ruby app/tc_ruby_app.rb'
-        sh 'ls -ltrh test/reports/'
-    }
+                stage('Test'){
+                    sh 'ruby app/tc_ruby_app.rb'
+                    sh 'ls -ltrh test/reports/'
+                }
 
-    stage('Teardown'){
-        sh 'docker-compose stop && docker-compose rm -f || true'
-        sh 'sudo docker rm -f opsschool_dummy_app || true'
-        sh 'sudo docker rm -f buildclassdummyproject_nginx_1 || true'
+                stage('Teardown'){
+                    sh 'docker-compose stop && docker-compose rm -f || true'
+                    sh 'sudo docker rm -f opsschool_dummy_app || true'
+                    sh 'sudo docker rm -f buildclassdummyproject_nginx_1 || true'
 
-    }
+                }
 
-    stage('Build'){
-        sh 'sudo docker build --no-cache -t localhost:5000/opsschool_dummy_app:latest .'
-        sh 'sudo docker push localhost:5000/opsschool_dummy_app:latest'
-    }
+                stage('Build'){
+                    sh 'sudo docker build --no-cache -t localhost:5000/opsschool_dummy_app:latest .'
+                    sh 'sudo docker push localhost:5000/opsschool_dummy_app:latest'
+                }
 
-    stage('Deploy'){
-        sh 'sudo docker pull localhost:5000/opsschool_dummy_app:latest'
-        sh 'sudo docker-compose up -d'
-    }
+                stage('Deploy'){
+                    sh 'sudo docker pull localhost:5000/opsschool_dummy_app:latest'
+                    sh 'sudo docker-compose up -d'
+                }
 
-    junit 'test/reports/*.xml'
+                junit 'test/reports/*.xml'
 
-}
+            }
+
+        },
+        "AwsStream":{
+            node ('ops-school-dynamic-slave'){
+                currentBuild.result = "SUCCESS"
+
+                stage('Checkout'){
+
+                    checkout scm
+                }
+
+                stage('Test'){
+                    sh 'ruby app/tc_ruby_app.rb'
+                    sh 'ls -ltrh test/reports/'
+                }
+
+                stage('Teardown'){
+                    sh 'docker-compose stop && docker-compose rm -f || true'
+                    sh 'sudo docker rm -f opsschool_dummy_app || true'
+                    sh 'sudo docker rm -f buildclassdummyproject_nginx_1 || true'
+
+                }
+
+                stage('Build'){
+                    sh 'sudo docker build --no-cache -t localhost:5000/opsschool_dummy_app:latest .'
+                    sh 'sudo docker push localhost:5000/opsschool_dummy_app:latest'
+                }
+
+                stage('Deploy'){
+                    sh 'sudo docker pull localhost:5000/opsschool_dummy_app:latest'
+                    sh 'sudo docker-compose up -d'
+                }
+
+                junit 'test/reports/*.xml'
+
+            }
+
+        }
+)
